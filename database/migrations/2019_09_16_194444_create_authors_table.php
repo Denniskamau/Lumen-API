a83f1b83ecf3ad6d16b273687af876659a1cf578
Dennis <denniskamau3@gmail.com>
2019-09-16 22:50:06 +0300

    initial commit

---
+++ database/migrations/2019_09_16_194444_create_authors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('email');
+            $table->string('github');
+            $table->string('twitter');
+            $table->string('location');
+            $table->string('latest_article_published');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
